1d11d7a838bfa6ecc7e1a4740e045d06f6a66a65
benpankow <ben@dagsterlabs.com>
2024-05-30 14:51:42 -0700

    [blueprints] add VS Code schema-installing CLI command

---
+++ examples/experimental/dagster-blueprints/dagster_blueprints_tests/test_cli/sample_blueprint_project/blueprint_project/__init__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from dagster import asset
+from dagster_blueprints.blueprint import Blueprint, BlueprintDefinitions
+from dagster_blueprints.load_from_yaml import YamlBlueprintsLoader
+
+
+class SimpleAssetBlueprint(Blueprint):
+    key: str
+
+    def build_defs(self) -> BlueprintDefinitions:
+        @asset(key=self.key)
+        def blueprint_asset(): ...
+
+        return BlueprintDefinitions(assets=[blueprint_asset])
+
+
+loader = YamlBlueprintsLoader(
+    path=Path(__file__).parent / "blueprints", per_file_blueprint_type=SimpleAssetBlueprint
+)
